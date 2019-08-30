22e1ceaafc4958f1b77bd2e4ea244f67c87b6b2b
lily <marcela.blanco@youse.com.co>
2019-08-29 21:57:00 -0500

    Add Router and create Sign_up view

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Route } from "react-router-dom";
 import Login from './user/Login';
+import Sign_up from './user/Sign_up'
+
 
 
 const App = () => (
   <div>
-    <Login/>
+    <BrowserRouter>
+      <Route path="/sign_in" component={Login}/>
+      <Route path="/sign_up" component={Sign_up}/>
+    </BrowserRouter>
   </div>
 )
 
